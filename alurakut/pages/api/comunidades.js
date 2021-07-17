22493d741aa76a88487170ec36f0eb47a89dd001
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-07-17 13:25:05 -0300

    Imersão Alura, Recriada a página do Orkut

---
+++ alurakut/pages/api/comunidades.js
@@ -0,0 +1,25 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+  if (request.method === 'POST') {
+    const TOKEN = '3a9782340b8b4df0d359bb792b880b';
+    const client = new SiteClient(TOKEN);
+
+    const registroCriado = await client.items.create({
+      itemType: '836335',
+      ...request.body,
+    });
+
+    console.log(registroCriado);
+
+    response.json({
+      dados: 'Algum dado qualquer',
+      registroCriado: registroCriado,
+    });
+    return;
+  }
+
+  response.status(404).json({
+    message: 'Ainda não temos nada no GET, mas no POST tem!',
+  });
+}
